2617711eec433a7f027440992fc8148cf0ac24ad
tranphuhao2311 <tran.phu.hao.it@gmail.com>
2017-01-11 21:47:51 +0700

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,8 @@
+<?php
+	$servername="localhost";
+	$username="root";
+	$password="";
+	$dbname="cuahangdienthoai";
+	$conn= new mysqli($servername,$username,$password,$dbname);
+	mysqli_set_charset($conn, 'UTF8');
+?>
