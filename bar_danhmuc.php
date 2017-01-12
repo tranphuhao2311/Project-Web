c2852370ab56ed08ba39630078611d87d89aa6b2
tranphuhao2311 <tran.phu.hao.it@gmail.com>
2017-01-12 14:02:45 +0700

    Add files via upload

---
+++ bar_danhmuc.php
@@ -0,0 +1,15 @@
+<?php
+	echo'<div class="container-fluid new-menu">
+  <div class="row">
+    <div class="container container-fixed">
+      <div class="row" style="position: relative;">
+        <div class="new-btn-menu" onClick="puMenu();">
+          <div class="l-n-m"> <i class="icons mn" title="ViettelStore.vn"></i> </div>
+          <div class="r-n-m">DANH MỤC</div>
+          <input type="hidden" value="0" id="status-btnmenu">
+        </div>
+      </div>
+    </div>
+  </div>
+</div>';
+?>
