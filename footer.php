d65802ed9f72dbe7df07d306ace4b72f61f4b769
tranphuhao2311 <tran.phu.hao.it@gmail.com>
2017-01-12 16:35:15 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,157 @@
+﻿<footer class="container-fluid">
+  <div class="row">
+    <div class="container container-fixed">
+      <div class="row cam-ket">
+        <div class="col-lg-6">
+          <div class="row item2">
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+              <div class="row">
+                <div class="col-lg-5 col-md-3 col-sm-4 col-xs-4 it-cam-ket"> <i class="icons ck06"></i> </div>
+                <div class="col-lg-7 col-md-9 col-sm-8 col-xs-8">
+                  <div class="row">
+                    <h5>Sản phẩm</h5>
+                    <h4>CHÍNH HÃNG</h4>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+              <div class="row">
+                <div class="col-lg-5 col-md-3 col-sm-4 col-xs-4 it-cam-ket"> <i class="icons ck08"></i> </div>
+                <div class="col-lg-7 col-md-9 col-sm-8 col-xs-8">
+                  <div class="row">
+                    <h5>Giao hàng tận nơi</h5>
+                    <h4>63 TỈNH THÀNH</h4>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-6">
+          <div class="row item2">
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+              <div class="row">
+                <div class="col-lg-5 col-md-3 col-sm-4 col-xs-4 it-cam-ket"> <i class="icons ck10"></i> </div>
+                <div class="col-lg-7 col-md-9 col-sm-8 col-xs-8">
+                  <div class="row">
+                    <h5>Tư vấn miễn phí</h5>
+                    <a href="tel:18008123">
+                    <h4>1800 8123</h4>
+                    </a> </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+              <div class="row">
+                <div class="col-lg-5 col-md-3 col-sm-4 col-xs-4 it-cam-ket"> <i class="icons ck03"></i> </div>
+                <div class="col-lg-7 col-md-9 col-sm-8 col-xs-8">
+                  <div class="row">
+                    <h5>Chính sách đổi trả</h5>
+                    <h4>LINH HOẠT</h4>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-7 col-md-12">
+          <div class="row">
+            <div class="col-xs-4">
+              <div class="row">
+                <h4>CHĂM SÓC KHÁCH HÀNG</h4>
+                <a href="tel:18008123">
+                <p> <span class="color_ff6600">1800 8123 </span>: Tư vấn bán hàng </p>
+                </a> <a href="tel:19008096">
+                <p> <span class="color_ff6600">1900 8096 </span>: Khiếu nại, Bảo hành </p>
+                </a>
+                <p> Thời gian làm việc: <span class="color_ff6600">8h - 22h </span> </p>
+                <a href="mailto:hanhntp@viettelstore.vn">
+                <p>Góp ý: <span class="color_333333">tran.phu.hao.it@gmail.com </span></p>
+                </a> </div>
+            </div>
+            <div class="col-xs-4">
+              <div class="row">
+                <h4>HỖ TRỢ KHÁCH HÀNG</h4>
+                <a href="#">
+                <p>Những câu hỏi thường gặp</p>
+                </a> <a href="#">
+                <p>Phương thức thanh toán</p>
+                </a> <a href="#">
+                <p>Hướng dẫn đặt hàng</p>
+                </a> <a href="#">
+                <p>Tra cứu đơn hàng trực tuyến</p>
+                </a> <a href="#">
+                <p style="color: red !important;">Tìm trung tâm bảo hành</p>
+                </a> </div>
+            </div>
+            <div class="col-xs-4">
+              <div class="row">
+                <h4>CHÍNH SÁCH</h4>
+                <a href="#">
+                <p>Chính sách giao hàng</p>
+                </a> <a href="#">
+                <p>Chính sách đổi sản phẩm</p>
+                </a> <a href="#">
+                <p>Chính sách bảo hành</p>
+                </a> <a href="#">
+                <p>Chính sách bảo mật</p>
+                </a> </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-5 col-md-12">
+          <div class="row">
+            <div class="col-xs-8">
+              <div class="row">
+                <h4>VIETTEL STORE</h4>
+                <div class="col-xs-6">
+                  <div class="row"> <a href="#">
+                    <p style="color: red;">Siêu thị gần nhất (291)</p>
+                    </a> <a href="#">
+                    <p>Tư vấn sử dụng</p>
+                    </a> <a href="#">
+                    <p>Đánh giá sản phẩm</p>
+                    </a> <a href="#">
+                    <p>Bệnh viện công nghệ</p>
+                    </a> <a href="#">
+                    <p>Liên hệ</p>
+                    </a> </div>
+                </div>
+                <div class="col-xs-6">
+                  <div class="row"> <a href="#">
+                    <p>Tin công nghệ</p>
+                    </a> <a href="#">
+                    <p>Tin khuyến mãi</p>
+                    </a> <a href="#">
+                    <p>Tin tuyển dụng</p>
+                    </a> <a href="#">
+                    <p>Tin Viettel</p>
+                    </a> </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-xs-4">
+              <div class="row">
+                <h4>LIÊN KẾT</h4>
+                <p class="group-xh"> <a href="#"> <i class="icons f"></i> </a> <a href="#"> <i class="icons g"></i> </a> <a href="#"> <i class="icons a"></i> </a> </p>
+                <h4 style="margin-top: 10px; margin-bottom: 0;">CHỨNG NHẬN</h4>
+                <a href="#">
+                <p> <i class="icons cnbct" title="Đã đăng ký với bộ công thương"></i> </p>
+                </a> </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row line">
+        <h4>CÔNG TY THƯƠNG MẠI VÀ XUẤT NHẬP KHẨU </h4>
+        <p>Địa chỉ: 227 Nguyễn Văn Cừ, Q5, TP HCM</p>
+        <p> Coppyright © 2017 </p>
+        <p> <a href="#"><span class="color_ff6600"> <i class="icons mb"></i> Xem bản mobile </span></a> </p>
+      </div>
+      <div class="row"> </div>
+    </div>
+  </div>
+</footer>
